e7b409f2d3a5ff3b96ec10103c6dc98d06952082
IrisMaeWCCI <1miss1hicks1@gmail.com>
2017-06-28 15:03:25 -0400

    added switch case practice

---
+++ WordsToDigits/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsToDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number in written form");
+
+            string userNumber = "seven";
+
+            switch (userNumber)
+            {
+                case zero:
+                    Console.WriteLine("You have entered 0");
+                    break;
+                case one:
+                    Console.WriteLine("You have entered 1");
+                    break;
+                case two:
+                    Console.WriteLine("You have entered 2");
+                    break;
+                case three:
+                    Console.WriteLine("You have entered 3");
+                    break;
+                case four:
+                    Console.WriteLine("You have entered 4");
+                    break;
+                case five:
+                    Console.WriteLine("You have entered 5");
+                    break;
+                case six:
+                    Console.WriteLine("You have entered 6");
+                    break;
+                case seven:
+                    Console.WriteLine("You have entered 7");
+                    break;
+                case eight:
+                    Console.WriteLine("You have entered 8");
+                    break;
+                case nine:
+                    Console.WriteLine("You have entered 9");
+                    break;
+                case ten:
+                    Console.WriteLine("You have entered 10");
+                    break;
+            }
+        }
+    }
+}
